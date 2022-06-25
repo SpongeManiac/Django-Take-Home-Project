4248d0a3211b4671cdbfe85498306aa65c642936
SpongeManiac <78384369+SpongeManiac@users.noreply.github.com>
2022-06-25 15:52:33 -0600

    Removed unused package
    Removed django-cryptography from imports and requirements. I did not use it and didn't want to bother with migrating existing entries. It can be re-added easily.

--- CRUD_example/models.py
+++ CRUD_example/models.py
@@ -3,7 +3,6 @@ from rest_framework.authtoken.models import Token
 
 from django.db import models
 from django.dispatch import receiver
-from django_cryptography.fields import encrypt
 from django.db.models.signals import post_save
 from django.contrib.auth.models import (
     BaseUserManager, AbstractBaseUser
